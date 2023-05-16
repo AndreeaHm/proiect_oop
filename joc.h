f346292b19d166a114547eb2ed9c2750da6e0f4d
AndreeaHm <92792751+AndreeaHm@users.noreply.github.com>
2023-05-16 11:23:42 +0300

    Create joc.h

---
+++ joc.h
@@ -0,0 +1,48 @@
+//
+// Created by Andreea on 13-May-23.
+//
+
+#ifndef PRJCT_OOP_JOC_H
+#define PRJCT_OOP_JOC_H
+
+#pragma once
+
+#include <vector>
+#include <string>
+#include <random>
+#include "Tara.h"
+#include "Jucator.h"
+#include "Continent.h"
+
+class Joc {
+
+
+    std::vector<Jucator> juc;
+    std::vector<std::string> raspunsuri;
+    std::vector<Continent*> continente;
+
+public:
+    Joc();
+
+    Joc(const std::vector<Jucator> &juc_, const std::vector<std::string> &raspunsuri_, const std::vector<Continent*> &continente_);
+
+    Joc(const Joc &other);
+
+    Joc(Joc &&other) noexcept;
+
+    Joc &operator=(const Joc &other);
+
+    Joc &operator=(Joc &&other) noexcept;
+
+    friend std::ostream &operator<<(std::ostream &os, const Joc &joc);
+
+    void adauga_jucator(const Jucator &jucator);
+
+    void adaugaContinent(const Continent& continentu);
+
+    void joaca(Joc &joc);
+
+    ~Joc();
+};
+
+#endif //PRJCT_OOP_JOC_H
