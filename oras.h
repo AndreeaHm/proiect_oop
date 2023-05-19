527575b79e1eb23350e90d235b3e62cb3c04e0d1
AndreeaHm <92792751+AndreeaHm@users.noreply.github.com>
2023-05-19 14:24:03 +0300

    Create oras.h

---
+++ oras.h
@@ -0,0 +1,25 @@
+//
+// Created by Andreea on 16-May-23.
+//
+
+#ifndef PRJCT_OOP_ORAS_H
+#define PRJCT_OOP_ORAS_H
+
+#include <string>
+#include <iostream>
+#include "entitate.h"
+
+class Oras : public Entitate {
+    int population;
+
+public:
+    Oras(const std::string& name_, int population_);
+    Oras(const Oras& other);
+    ~Oras();
+
+    friend std::ostream& operator<<(std::ostream& os, const Oras& oras);
+
+    int getPopulation() const;
+};
+
+#endif //PRJCT_OOP_ORAS_H
