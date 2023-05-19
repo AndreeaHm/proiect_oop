89d4671539946e632f4af9251c36b0e37c24354f
AndreeaHm <92792751+AndreeaHm@users.noreply.github.com>
2023-05-19 14:24:29 +0300

    Create oras.cpp

---
+++ oras.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Andreea on 16-May-23.
+//
+
+#include "oras.h"
+
+Oras::Oras(const std::string& name_, int population_)
+        : Entitate(name_), population(population_) {}
+
+Oras::Oras(const Oras& other)
+        : Entitate(other), population(other.population) {}
+
+Oras::~Oras() {}
+
+std::ostream& operator<<(std::ostream& os, const Oras& oras) {
+    os << "Nume oras: " << oras.getNume() << std::endl;
+    os << "Populatie: " << oras.population << std::endl;
+    return os;
+}
+
+int Oras::getPopulation() const {
+    return population;
+}
